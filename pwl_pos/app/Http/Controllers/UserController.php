cc8e46c5a1f3a24cbfec5655f8aaff283a0bb900
Ilham FT <ilhamft.magang.om@jagoanhosting.com>
2025-03-08 20:55:11 +0700

    remove pwl_pos from minggu 3

--- pwl_pos/app/Http/Controllers/UserController.php
+++ pwl_pos/app/Http/Controllers/UserController.php
@@ -12,10 +12,13 @@ class UserController extends Controller
     public function index()
     {
         $data = [
-            'nama' => 'Pelanggan Pertama',
+            'level_id' => 2,
+            'username' => 'manager_dua',
+            'nama' => 'Manager 2',
+            'password' => Hash::make('12345678'),
         ];
 
-        UserModel::where('username', 'customer-1')->update($data);
+        UserModel::create($data);
 
         $user = UserModel::all(); // mengambil data dari table m_user
         return view('user', ['data' => $user]);
