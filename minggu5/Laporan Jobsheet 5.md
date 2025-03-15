85c610f480d57f423e5afaa9e379cd30467f14ca
Ilham FT <ilhamft.magang.om@jagoanhosting.com>
2025-03-16 00:15:37 +0700

    Jobsheet 5 - Praktikum 2

--- minggu5/Laporan Jobsheet 5.md
+++ minggu5/Laporan Jobsheet 5.md
@@ -1,4 +1,4 @@
-# Laporan Praktikum 1 - $fillable
+# Laporan Jobsheet 5
 
 **Nama**  : Ilham Faturachman  
 **NIM**   : 244107023001  
@@ -13,3 +13,15 @@
 Dengan menggunakan `composer require jeroennoten/laravel-adminlte` maka akan mendownload adminlte ke dalam project Laravel. setelah kita bisa menjalankan `php artisan adminlte:install` untuk menginstal adminlte.
 
 ![Hasil dengan composer](Screenshot%20Laporan/Praktikum1/praktikum1.png)
+
+---
+
+## 📌 Praktikum 2 - Integrasi Laravel DataTable  
+
+### 1️⃣ Instalasi Laravel DataTable ✅
+Dengan menggunakan `composer require yajra/laravel-datatables` maka akan mendownload laravel-datatables ke dalam project Laravel. 
+
+### 2️⃣ Integrasi DataTable ✅
+dengan menggunakan perintah `php artisan make:datatables KategoriDataTable` maka akan membuat file `KategoriDataTable.php` di dalam folder `app/DataTables`. dan itu bisa digunakan untuk menampilkan data kategori dalam tabel.
+
+![Hasil dengan composer](Screenshot%20Laporan/Praktikum2/praktikum2.png)
--- minggu5/pwl_pos/app/Http/Controllers/KategoriController.php
+++ minggu5/pwl_pos/app/Http/Controllers/KategoriController.php
@@ -2,32 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\DataTables\KategoriDataTable;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class KategoriController extends Controller
 {
-    public function index()
+    public function index(KategoriDataTable $dataTable)
     {
-        // $data = [
-        //     'kategori_kode' => 'SNK',
-        //     'kategori_name' => 'Snack/Makanan Ringan',
-        //     'created_at' => now(),
-        // ];
-
-        // DB::table('m_kategori')->insert($data);
-        // return 'Insert data baru berhasil';
-
-        // $row = DB::table('m_kategori')->where('kategori_kode', 'SNK')->update([
-        //     'kategori_name' => 'Camilan',
-        //     'updated_at' => now(),
-        // ]);
-        // return "update data berhasil, affected rows: $row";
-
-        // $row = DB::table('m_kategori')->where('kategori_kode', 'SNK')->delete();
-        // return "delete data berhasil, affected rows: $row";
-
-        $data = DB::table('m_kategori')->get();
-        return view('kategori', ['data' => $data]);
+        return $dataTable->render('kategori.index');
     }
 }
