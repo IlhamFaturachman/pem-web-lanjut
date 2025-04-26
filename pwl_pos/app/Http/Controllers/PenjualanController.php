5111726f81216f91bd3afe3b89b309f0af3133c4
Ilham FT <ilhamft.magang.om@jagoanhosting.com>
2025-04-26 23:50:29 +0700

    PWL UTS Lupa Push

--- pwl_pos/app/Http/Controllers/PenjualanController.php
+++ pwl_pos/app/Http/Controllers/PenjualanController.php
@@ -68,8 +68,6 @@ class PenjualanController extends Controller
             })
             ->addColumn('aksi', function ($row) {
                 $btn = '<button onclick="modalAction(\'' . url('/penjualan/' . $row->penjualan_id . '/show_ajax') . '\')" class="btn btn-info btn-sm">Detail</button> ';
-                $btn .= '<button onclick="modalAction(\'' . url('/penjualan/' . $row->penjualan_id . '/edit_ajax') . '\')" class="btn btn-warning btn-sm">Edit</button> ';
-                $btn .= '<button onclick="modalAction(\'' . url('/penjualan/' . $row->penjualan_id . '/delete_ajax') . '\')" class="btn btn-danger btn-sm">Hapus</button> ';
                 return $btn;
             })
             ->rawColumns(['aksi'])
--- pwl_pos/app/Http/Controllers/WelcomeController.php
+++ pwl_pos/app/Http/Controllers/WelcomeController.php
@@ -8,16 +8,38 @@ class WelcomeController extends Controller
 {
     public function index()
     {
+        $user = auth()->user(); // Ambil user login
         $breadcrumb = (object)[
             'title' => 'Selamat Datang',
             'list' => ['Home', 'Welcome'],
         ];
 
         $activeMenu = 'dashboard';
-        
-        return view('welcome', [
-            'breadcrumb' => $breadcrumb,
-            'activeMenu' => $activeMenu,
-        ]);
+
+        // Cek role dan arahkan ke view berbeda
+        switch ($user->level->level_kode) {
+            case 'ADM':
+                return view('dashboard.admin', [
+                    'breadcrumb' => $breadcrumb,
+                    'activeMenu' => $activeMenu,
+                ]);
+            case 'MNG':
+                return view('dashboard.manager', [
+                    'breadcrumb' => $breadcrumb,
+                    'activeMenu' => $activeMenu,
+                ]);
+            case 'STF':
+                return view('dashboard.staff', [
+                    'breadcrumb' => $breadcrumb,
+                    'activeMenu' => $activeMenu,
+                ]);
+            case 'CUS':
+                return view('dashboard.customer', [
+                    'breadcrumb' => $breadcrumb,
+                    'activeMenu' => $activeMenu,
+                ]);
+            default:
+                abort(403, 'Role tidak dikenali');
+        }
     }
 }
