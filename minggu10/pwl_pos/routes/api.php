7f0389a6d6dfbff6569283c03d6cda1cb3c6c724
Ilham FT <ilhamft.magang.om@jagoanhosting.com>
2025-04-27 10:34:00 +0700

    Jobsheet 10 - Implementasi Tugas

--- minggu10/pwl_pos/routes/api.php
+++ minggu10/pwl_pos/routes/api.php
@@ -1,9 +1,12 @@
 <?php
 
+use App\Http\Controllers\Api\BarangController;
+use App\Http\Controllers\Api\KategoriController;
 use App\Http\Controllers\Api\LevelController;
 use App\Http\Controllers\Api\LoginController;
 use App\Http\Controllers\Api\LogoutController;
 use App\Http\Controllers\Api\RegisterController;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -22,6 +25,9 @@ Route::post('/register', RegisterController::class)->name('register');
 Route::post('/login', LoginController::class)->name('login');
 Route::post('/logout', LogoutController::class)->name('logout');
 Route::apiResource('levels', LevelController::class);
+Route::apiResource('kategoris', KategoriController::class);
+Route::apiResource('barangs', BarangController::class);
+Route::apiResource('users', UserController::class);
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
